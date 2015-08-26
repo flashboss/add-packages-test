992d9a63d5d2ca1b5d2c5bd348fddfe43fce0ad9
flashboss <luca.stancapiano@vige.it>
2015-08-26 22:31:37 +0200

    first commit

---
+++ src/test/java/it/vige/arquillian/AddPackagesTestCase.java
@@ -0,0 +1,26 @@
+package it.vige.arquillian;
+
+import static org.junit.Assert.assertTrue;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class AddPackagesTestCase {
+
+	@Deployment
+	public static JavaArchive createDeployment() {
+		JavaArchive javaArchive = ShrinkWrap.create(JavaArchive.class).addPackages(true, "org");
+		assertTrue(javaArchive.toString(true).contains("h2"));
+		return javaArchive;
+	}
+
+	@Test
+	public void emptyTest() {
+
+	}
+}
